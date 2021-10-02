450cb10006f8d23ea5791708b70f2b4991f02a32
LukeG007 <luke@gilsinger.net>
2021-10-02 17:28:55 -0400

    Change dark red theme

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def home():
         pass
     else:
         cookies['theme'] = 'light.css'
-    if cookies['theme'] == 'dark.css':
+    if cookies['theme'] == 'dark.css' or cookies['theme'] == 'dark_red.css':
         darkmode = True
     else:
         darkmode = False

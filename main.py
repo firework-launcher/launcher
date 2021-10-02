39f822baaf2eb0d54159768dd424e63389e26b71
LukeG007 <luke@gilsinger.net>
2021-10-01 22:51:31 -0400

    Added experimental branch

--- main.py
+++ main.py
@@ -168,5 +168,6 @@ def reset():
     return redirect('/')
 
 if __name__ == '__main__':
+    print('WARNING: This is the experimental branch and some features may not work as intended')
     threading.Thread(target=firework_serial_write).start()
     app.run(host='0.0.0.0', port=80)

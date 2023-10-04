615390300c3223938996fa3afed58c412ad1d22c
lukeg007 <luke@gilsinger.net>
2023-10-03 20:38:49 -0400

    Add close buttons to warnings

--- static/js/checknewlauncher.js
+++ static/js/checknewlauncher.js
@@ -5,6 +5,14 @@ socket.on("add_node_discover", (data) => {
 });
 
 socket.on("node_crash_warning", (data) => {
-    document.getElementById("node_warn_crash_txt").innerText = data[1] + " (" + data[0] + ") restarted. Some fireworks may have not been lit.";
-    document.getElementById("node_warn_crash").setAttribute("style", "display: block")
+    document.getElementById("node_warn_crash_txt").innerHTML = data[1] + " (" + data[0] + `) restarted. Some fireworks may have not been lit. <span><button class="yellow_close_button" onclick="close_warn_crash();">X</button></span>`;
+    document.getElementById("node_warn_crash").setAttribute("style", "display: block");
 });
+
+function close_warn_crash() {
+    document.getElementById("node_warn_crash").setAttribute("style", "");
+}
+
+function close_add_node() {
+    document.getElementById("node_warn_added").setAttribute("style", "");
+}
--- static/js/main.js
+++ static/js/main.js
@@ -539,8 +539,14 @@ socket.on("add_node_discover", async (data) => {
 });
 
 socket.on("node_crash_warning", (data) => {
-    document.getElementById("node_warn_crash_txt").innerText = data[1] + " (" + data[0] + ") restarted. Some fireworks may have not been lit.";
-    document.getElementById("node_warn_crash").setAttribute("style", "display: block")
+    document.getElementById("node_warn_crash_txt").innerHTML = data[1] + " (" + data[0] + `) restarted. Some fireworks may have not been lit. <span><button class="yellow_close_button" onclick="close_warn_crash();">X</button></span>`;
+    document.getElementById("node_warn_crash").setAttribute("style", "display: block");
 });
+
+function close_warn_crash() {
+    document.getElementById("node_warn_crash").setAttribute("style", "");
+}
+
+
 
 reload_all()
